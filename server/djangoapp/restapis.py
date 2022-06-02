bc64c244d8ec27e50372e7d5068ea48415bacaf0
fiusks <rafabarros1@hotmail.com>
2022-06-02 12:27:46 -0300

    NLU implementation

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,21 +1,29 @@
-from email import message
-from unittest import result
 import requests
 import json
-from django.http import HttpResponseRedirect, HttpResponse
 from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
-# from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-# from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions, EntitiesOptions, KeywordsOptions
 
 
 def get_request(url, **kwargs):
-    print(kwargs)
+    api_key = kwargs.get('apiKey')
     print("GET from {} ".format(url))
     try:
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                params=kwargs)
+        if api_key:
+            params = dict()
+            params["text"] = kwargs["text"]
+            params["version"] = kwargs["version"]
+            params["features"] = kwargs["features"]
+            params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+            response = requests.get(
+                url, params=params,
+                headers={'Content-Type': 'application/json'},
+                auth=HTTPBasicAuth('apikey', api_key))
+        else:
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
     except:
         print("Network exception occurred")
     status_code = response.status_code
@@ -58,11 +66,31 @@ def get_dealers_by_state_from_cf(url, state):
     return results
 
 
+def analyze_review_sentiments(dealerreview):
+    url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/1a31bf71-5ded-4c6c-bad0-84a1bd18fcb9"
+    api_key = "hla2WzbVV2swmwgyc9KptJyUkvMN8qNhR0Dxwdz8pEDw"
+    authenticator = IAMAuthenticator(api_key)
+
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version='2022-04-07',
+        authenticator=authenticator
+    )
+
+    natural_language_understanding.set_service_url(url)
+
+    response = natural_language_understanding.analyze(text=dealerreview, features=Features(
+        sentiment=SentimentOptions(targets=[dealerreview]))).get_result()
+    sentiment = response['sentiment']['document']['label']
+    return sentiment
+
+
+analyze_review_sentiments("This dealer is very good. I'm excited.")
+
+
 def get_dealer_reviews_from_cf(url, dealerId):
     results = []
     json_result = get_request(url+"?dealerId="+str(dealerId))
     reviews = json_result["review"]
-    # sentiment = analyze_review_sentiments(review_obj.review)
 
     for dealer_review in reviews:
         if(dealer_review['purchase'] == False):
@@ -71,7 +99,7 @@ def get_dealer_reviews_from_cf(url, dealerId):
                 name=dealer_review["name"],
                 purchase=dealer_review["purchase"],
                 review=dealer_review["review"],
-                sentiment="teste")
+                sentiment=analyze_review_sentiments(dealer_review['review']))
         else:
             review_obj = DealerReview(
                 dealership=dealer_review["dealership"],
@@ -83,7 +111,7 @@ def get_dealer_reviews_from_cf(url, dealerId):
                 car_make=dealer_review["car_make"],
                 car_model=dealer_review["car_model"],
                 car_year=dealer_review["car_year"],
-                sentiment="teste"
+                sentiment=analyze_review_sentiments(dealer_review['review'])
             )
 
         results.append(review_obj)
